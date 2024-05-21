e01f7ce80ecd47d6a16ccc9cb509323084bbc644
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-05-20 22:03:35 -0400

    learn: custon middleware

---
+++ MiddlewareExample/MyCustomMiddleware.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MiddlewareExample;
+
+public class MyCustomMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        await next(context);
+    }
+}
